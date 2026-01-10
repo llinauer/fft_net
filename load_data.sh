38592a2001b36cd881891a37610d5bd1f850bafb
llinauer <l.linauer@gmx.at>
2026-01-10 20:15:21 +0100

    init uv project, create model.py, add load_data.sh

---
+++ load_data.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+# download
+wget https://data.caltech.edu/records/65de6-vp158/files/CUB_200_2011.tgz?download=1 -O CUB_200_2011.tgz
+# unpack
+tar -xf CUB_200_2011
---
+++ model.py
@@ -0,0 +1,6 @@
+import torch
+from torch import nn
+
+class FFTMLP(nn.Module):
+    """ FFT + MLP """
+    pass
