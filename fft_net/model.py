32390e5c9ac385ee4abb22daa07416c42f899e72
llinauer <l.linauer@gmx.at>
2026-01-18 16:46:19 +0100

    Add hydra

--- fft_net/model.py
+++ fft_net/model.py
@@ -5,8 +5,8 @@ class FFTMLP(nn.Module):
     """ FFT + MLP """
     
 
-    def __init__(self, dims: tuple[int] = (100, 50)):
+    def __init__(self, img_size: tuple[int, int], dims: tuple[int, ...] = (100, 50)):
         super.__init__()
-        input_dim = 
+        input_dim = img_size[0] * img_size[1]
         self.fc1 = nn.Linear()
         self.net = nn.Sequential(*[nn.Linear()])
